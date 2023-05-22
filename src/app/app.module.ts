e56d1a70062c87dc9203200f9243970f59a5a630
Rodrigo_Cordova_A <a01610371@tec.mx>
2023-05-22 10:19:09 -0600

    Correción y primera respuesta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,9 @@ import { ResultadosComponent } from './preguntas/resultados/resultados.component
     MatRadioModule,
     MatButtonModule,
     MatDialogModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+
     
   ],
   providers: [DataService],
--- src/app/preguntas/resultados/resultados.component.ts
+++ src/app/preguntas/resultados/resultados.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css']
 })
-export class ResultadosComponent {
+export class ResultadosComponent implements OnInit{
 
+  constructor(
+    public dialogRef: MatDialogRef<ResultadosComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
