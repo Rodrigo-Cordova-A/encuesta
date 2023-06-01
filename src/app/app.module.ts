e5d902314dde07c966a90698197449552670a042
Rodrigo_Cordova_A <a01610371@tec.mx>
2023-05-31 21:19:17 -0600

    Avance Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -34,6 +34,8 @@ import { ResultadosComponent } from './preguntas/resultados/resultados.component
     MatDialogModule,
     FormsModule,
     MatIconModule,
+    MatGridListModule,
+    
 
     
   ],
